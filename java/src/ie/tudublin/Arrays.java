0f3af8bc3fb62a05f543912124a0feb0017220a4
c22447592 <c22447592@mytudublin.ie>
2024-02-14 16:21:25 +0000

    trend graph done :D

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -6,9 +6,11 @@ import processing.core.PApplet;
 
 public class Arrays extends PApplet
 {
+	int mode = 0;
+
 	String[] months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
 
-	float[] rainfall = {45, 37, 55, 25, 37, 40, 50, 80, 50, 105, 30, 60};
+	float[] rainfall = {45, 37, 55, 25, 37, 50, 80, 50, 105, 30, 100, 60};
 
 	float lenYaxis = 400;
 	float lenXaxis = 400;
@@ -96,38 +98,94 @@ public class Arrays extends PApplet
 	
 	public void draw()
 	{	
-
-		background(0);
-
-		//draw graph axis
-		stroke(0, 0, 255);
-		line(50, 50, 50, 450);
-		line(50, 450, 450, 450);
-		
-		//draw graph labels to the left of the y axis with the rainfall values from 0 to 120 increasing in 10s
-		for(int i = 0 ; i <= 120 ;  i += 10)
+		switch(mode)
 		{
-			fill(0,0, 255); // sets the colour of the text
-			float y = map1(i, 0, 120, 450, 50); // maps the rainfall values to the height of the graph
-			textAlign(RIGHT, CENTER); // sets the alignment of the text
-			text(i, 40, y); // draws the rainfall values to the left of the y axis
+			case 0:
+			break;
+
+			case 1: //first mode
+				background(0);
+
+				//draw graph axis
+				stroke(0, 0, 255);
+				line(50, 50, 50, 450);
+				line(50, 450, 450, 450);
+				
+				//draw graph labels to the left of the y axis with the rainfall values from 0 to 120 increasing in 10s
+				for(int i = 0 ; i <= 120 ;  i += 10)
+				{
+					fill(0,0, 255); // sets the colour of the text
+					float y = map1(i, 0, 120, 450, 50); // maps the rainfall values to the height of the graph
+					textAlign(RIGHT, CENTER); // sets the alignment of the text
+					text(i, 40, y); // draws the rainfall values to the left of the y axis
+				}
+
+				//draw the graph
+				float w = lenXaxis / (float)months.length; // sets the width of the bars to the width of the graph divided by the number of months
+				float hue = 0; // sets the hue value to 0
+
+				//draw the bars with the months underneath the x axis
+				for(int i = 0; i < months.length; i ++)
+				{
+					float x = map1(i, 0, months.length, 50, 450); // maps the months to the width of the graph
+					float h = map1(rainfall[i], 0, 120, 0, 400); // maps the rainfall values to the height of the graph
+					fill(hue, 255, 255); // sets the colour of the bars
+					rect(x, 450 - h, w, h); // draws the bars
+					fill(0,0,255); // sets the colour of the text
+					textAlign(CENTER, CENTER); // sets the alignment of the text
+					text(months[i], x + w * 0.5f, 460); // draws the months underneath the x axis
+					hue += 20; // increments the hue value
+				}
+			break;
+
+			case 2:
+				background(0);
+
+				//draw graph axis
+				stroke(0, 0, 255);
+				line(50, 50, 50, 450);
+				line(50, 450, 450, 450);
+
+				//draw graph labels to the left of the y axis with the rainfall values from 0 to 120 increasing in 10s
+				for(int i = 0 ; i <= 120 ;  i += 10)
+				{
+					fill(0,0, 255); // sets the colour of the text
+					float y = map1(i, 0, 120, 450, 50); // maps the rainfall values to the height of the graph
+					textAlign(RIGHT, CENTER); // sets the alignment of the text
+					text(i, 40, y); // draws the rainfall values to the left of the y axis
+				}
+
+				//draw trend lines and months underneath the x axis
+				float w1 = lenXaxis / (float)months.length; // sets the width of the bars to the width of the graph divided by the number of months
+				for (int i = 1; i < months.length; i++)
+				{
+					float x1 = map1(i-1, 0, months.length, 50, 450); // maps the months to the width of the graph
+					float y1 = map1(rainfall[i-1], 0, 120, 0, 400); // maps the rainfall values to the height of the graph
+					float x2 = map1(i, 0, months.length, 50, 450); // maps the next month to the width of the graph
+					float y2 = map1(rainfall[i], 0, 120, 0, 400); // maps the next rainfall value to the height of the graph
+
+
+					stroke(0, 255, 255); // sets the colour of the trend lines
+					line(x1 + w1 * 0.5f, 450 - y1, x2 + w1 * 0.5f, 450 - y2); // draws the trend lines
+					fill(0,0,255); // sets the colour of the text
+					textAlign(CENTER, CENTER); // sets the alignment of the text
+					text(months[i-1], x1 + w1 * 0.5f, 460); // draws the months underneath the x axis
+					if (i == months.length - 1) // if the month is the last month
+					{
+						text(months[i], x2 + w1 * 0.5f, 460); // draws the months underneath the x axis
+					}
+				}
+			break;
+				
 		}
-
-		//draw the graph
-		float w = lenXaxis / (float)months.length; // sets the width of the bars to the width of the graph divided by the number of months
-		float hue = 0; // sets the hue value to 0
-
-		//draw the bars with the months underneath the x axis
-		for(int i = 0; i < months.length; i ++)
-    {
-        float x = map1(i, 0, months.length, 50, 450); // maps the months to the width of the graph
-        float h = map1(rainfall[i], 0, 120, 0, 400); // maps the rainfall values to the height of the graph
-        fill(hue, 255, 255); // sets the colour of the bars
-        rect(x, 450 - h, w, h); // draws the bars
-        fill(0,0,255); // sets the colour of the text
-        textAlign(CENTER, CENTER); // sets the alignment of the text
-        text(months[i], x + w * 0.5f, 460); // draws the months underneath the x axis
-        hue += 18; // increments the hue value
-    }
 	}
+
+	public void keyPressed()
+    {
+        
+        if (key >= '0' && key <= '9')
+        {
+            mode = key - '0';
+        }
+    }
 }
