4013ae5fd58e10299a7cd69748bbc090c1f18156
c22447592 <c22447592@mytudublin.ie>
2024-02-14 15:03:33 +0000

    starting code

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -2,41 +2,27 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
+
+
 public class Arrays extends PApplet
 {
-	String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-	String minMonth = months[0];
-	String maxMonth = months[0];
+	String[] months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
 
 	float[] rainfall = {200, 260, 300, 150, 100, 50, 10, 40, 67, 160, 400, 420};
-	float min = rainfall[0];
-	float max = rainfall[0];
-	float avg = 0;
-
 
 	public float map1(float a, float b, float c, float d, float e)
-		{
-			float range1 = c - b;
-			float range2 = e - d;
-			float value = a - b;
-			float ratio = value / range1;
-			return d + range2 * ratio;
-		}
-	
-	public void drawBarchart()
 	{
-		colorMode(HSB);
-		// draw barchart for every month and their rainfall
-		for(int i = 0; i < months.length; i++)
-		{
-			float x = map1(i, 0, months.length, 50, 450); // maps i to a value between 50 and 450 (x-axis)
-			float y = map1(rainfall[i], 0, 500, 450, 50); // maps rainfall[i] to a value between 450 and 50 (y-axis)
-			float h = map1(rainfall[i], 0, 500, 0, 400); // maps rainfall[i] to a value between 0 and 400 (height of the bar)
-			fill(255); // sets the fill color to white
-			textAlign(CENTER, CENTER); // sets the text alignment to center
-			text(months[i], x, 475); // draws the month name at the bottom of the bar
-			fill(255, 255, 255); // sets the fill color to red
-			rect(x, y, 25, h); // draws the bar
+		float r1 = c -b;
+		float r2 = e - d;
+
+		float howFar = a - b;
+		return d + (howFar / r1) * r2;
+	}
+
+	void randomize()
+	{
+		for (int i = 0; i < rainfall.length; i++) {
+			rainfall[i] = random(500);
 		}
 	}
 
@@ -45,59 +31,77 @@ public class Arrays extends PApplet
 		size(500, 500);
 
 		String[] m1 = months;
-
-		for(int i = 0; i < months.length; i++)
+		months[0] = "XXX";
+		print(m1[0]);
+		for(int i = 0; i < months.length; i ++)
 		{
-			println("Month: "+ months[i] + "\t" + rainfall[i] + "mm");
+			println("Month: " + months[i] + "\t" + rainfall[i]);
 		}
-
 		for (String s : m1) {
 			println(s);
 		}
 
-		for (int i = 0; i < months.length; i++)
+		int minIndex = 0;
+		for(int i= 0 ; i < rainfall.length ; i ++)
 		{
-			if(min > rainfall[i])
+			if (rainfall[i] < rainfall[minIndex])
 			{
-				min = rainfall[i];
-				minMonth = months[i];
+				minIndex = i;
 			}
-			if(max < rainfall[i])
-			{
-				max = rainfall[i];
-				maxMonth = months[i];
-			}
-			avg = avg + rainfall[i];
 		}
-		avg = avg/months.length;
+		
+		int maxIndex = 0;
+		for(int i= 0 ; i < rainfall.length ; i ++)
+		{
+			if (rainfall[i] > rainfall[maxIndex])
+			{
+				maxIndex = i;
+			}
+		}
 
-		println("Minimum Rainfall: Month: " + minMonth + "\tRainfall: " + min);
-		println("Maximum Rainfall: Month: " + maxMonth + "\tRainfall: " + max);
-		println("Average Rainfall: " + avg + "mm");
+		println("The month with the minimum rainfall was " + months[minIndex] + " with " + rainfall[minIndex] + "rain");
+		println("The month with the max rainfall was " + months[maxIndex] + " with " + rainfall[maxIndex] + "rain");
+		
+		
+		float tot = 0;
+		for(float f:rainfall)
+		{
+			tot += f;
+		}
 
-		//a b-c d-e
+		float avg = tot / (float) rainfall.length;
+
+		// a b-c d-e;
 		println(map1(5, 0, 10, 0, 100));
+		// 50
 
 		println(map1(25, 20, 30, 200, 300));
+		// 250
 
 		println(map1(26, 25, 35, 0, 100));
+		// 10 
 
-		//draw barchart for months and rainfall
-		
 
 	}
 
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-
+		randomize();
 		
 		
 	}
 
 	
 	public void draw()
-	{
-		drawBarchart();
+	{	
+
+		background(0);
+		float w = width / (float)months.length;
+		for(int i = 0 ; i < months.length ;  i ++)
+		{
+			float x = map1(i, 0, months.length, 0, width);
+			rect(x, height, w, -rainfall[i]);
+		}
 	}
 }
